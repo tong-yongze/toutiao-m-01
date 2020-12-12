cb4e33af222b77a1ea2c529e926d7d0ab7eb9eb7
童永泽 <604232797@qq.com>
2020-12-12 22:12:18 +0800

    文章频道样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -7,11 +7,22 @@
     <!-- /导航栏 -->
 
     <!-- 频道列表 -->
-    <van-tabs v-model="active">
+    <!--
+      通过 v-model 绑定当前激活标签对应的索引值，默认情况下启用第一个标签。
+      通过 animated 属性可以开启切换标签内容时的转场动画。
+      通过 swipeable 属性可以开启滑动切换标签页。
+     -->
+    <van-tabs class="channel-tabs" v-model="active" animated swipeable>
       <van-tab title="标签 1">内容 1</van-tab>
       <van-tab title="标签 2">内容 2</van-tab>
       <van-tab title="标签 3">内容 3</van-tab>
       <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 5">内容 5</van-tab>
+      <van-tab title="标签 6">内容 6</van-tab>
+      <div slot="nav-right" class="placeholder"></div>
+      <div slot="nav-right" class="hamburger-btn">
+        <i class="font_family icon-gengduo"></i>
+      </div>
     </van-tabs>
     <!-- /频道列表 -->
   </div>
@@ -34,9 +45,9 @@ export default {
   methods: {},
 };
 </script>
-<style  lang="less">
+<style scoped lang="less">
 .home-container {
-  .van-nav-bar__title {
+  /deep/ .van-nav-bar__title {
     max-width: 100%;
     // 意思就是不要设置最大宽度
     // max-width: unset !important;
@@ -49,6 +60,60 @@ export default {
     font-size: 28px;
     .van-icon {
       font-size: 32px;
+    }
+  }
+
+  /deep/ .channel-tabs {
+    .van-tabs__wrap {
+      height: 82px;
+    }
+    .van-tab {
+      min-width: 200px;
+      border-right: 1px solid #edeff3;
+      font-size: 30px;
+      color: #777;
+    }
+    .van-tab--active {
+      color: #333;
+    }
+    .van-tabs__nav {
+      padding-bottom: 0;
+    }
+    .van-tabs__line {
+      bottom: 8px;
+      width: 31px !important;
+      height: 6px;
+      background-color: #3296fa;
+    }
+
+    .placeholder{
+      // 不让参加剩余空间计算
+       flex-shrink: 0;
+        width: 66px;
+        height: 82px;
+    }
+    .hamburger-btn {
+      position: fixed;
+      right: 0;
+      display: flex;
+      justify-content:center;
+      align-items: center;
+      width: 66px;
+      height: 82px;
+      background-color: #fff;
+      opacity: 0.902;
+      i.font_family {
+        font-size: 33px;
+      }
+      &::before {
+        content:'';
+        position: absolute;
+        left: 0;
+        width: 1px;
+        height: 100%;
+        background-image: url(~@/assets/gradient-gray-line.png);
+        background-size: contain;
+      }
     }
   }
 }
