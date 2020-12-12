dfbca0026ba7dfa4b31e59cf58e738013a568698
童永泽 <604232797@qq.com>
2020-12-12 20:55:43 +0800

    头部导航栏初始化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,22 @@
-<template >
+<template>
   <div class="home-container">
-    首页
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button class="search-btn" slot="title" type="info" size="small" round icon="search">搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 频道列表 -->
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- /频道列表 -->
   </div>
 </template>
+
 <script>
 export default {
   name: 'HomeIndex',
@@ -10,7 +24,7 @@ export default {
   props: {},
   data() {
     return {
-      active: 0,
+      active: 2,
     };
   },
   computed: {},
@@ -20,6 +34,22 @@ export default {
   methods: {},
 };
 </script>
-<style lang="">
-
+<style  lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: 100%;
+    // 意思就是不要设置最大宽度
+    // max-width: unset !important;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
 </style>
