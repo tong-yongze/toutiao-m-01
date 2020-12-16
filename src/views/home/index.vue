9228172140f02a13acb4e298a5fad9447938c3fe
童永泽 <604232797@qq.com>
2020-12-16 14:43:07 +0800

    获取展示首页频道列表

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -56,7 +56,9 @@
 import { getUserChannel } from '@/api/user';
 import ArticleList from './components/article-list';
 import ChannelEdit from './components/channel-edit'
-// import ChannelEdit from '@/views/home/components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
+
 export default {
   name: 'HomeIndex',
   components: {
@@ -71,7 +73,9 @@ export default {
       isChennelEditShow: true  // 控制编辑频道弹出层的显示状态
     };
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   created() {
     // 加载用户数据
@@ -79,11 +83,31 @@ export default {
   },
   mounted() {},
   methods: {
-    async loadChannels() {
+    // 请求用户的频道列表
+    async loadChannels () {
       try {
-        const { data } = await getUserChannel();
-        // console.log(data);
-        this.channels = data.data.channels;
+        // const { data } = await getUserChannel();
+        // // console.log(data);
+        // this.channels = data.data.channels;
+        let channels = []
+
+        if(this.user) {
+            // 已登录 请求获取用户频道列表
+          const { data } = await getUserChannel();
+          channels = data.data.channels
+        } else {
+             // 未登录 判断是否有本地地频道列表数据
+             const localChannels= getItem('TOUTIAO_CHANNELS')
+             // 有，拿来使用
+             if(localChannels) {
+               channels = loadChannels
+             } else {
+               // 木有就请求获取默认频道列表
+               const { data } = await getUserChannel()
+               channels = data.data.channels
+             }
+        }
+        this.channels = channels
       } catch (err) {
         this.$toast('获取频道数据失败!');
       }
