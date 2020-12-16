8126c1e0b4aa7895eb8375a30de53012b431d5dc
童永泽 <604232797@qq.com>
2020-12-16 22:49:16 +0800

    优化防抖debounce

---
+++ src/api/search.js
@@ -0,0 +1,14 @@
+/**
+ *  获取联想建议（自动补全）
+ */
+import request from '@/utils/request'
+
+export const getSearchSuggestions = q => {
+  return request({
+   method:'GET',
+   url:'/app/v1_0/suggestion',
+  params: {
+    q
+  }
+  })
+}
--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -1,25 +1,67 @@
 <template>
   <div class="search-suggestion">
-   <van-cell title="程序员..." icon="search"></van-cell>
-   <van-cell title="程序员..." icon="search"></van-cell>
-   <van-cell title="程序员..." icon="search"></van-cell>
-   <van-cell title="程序员..." icon="search"></van-cell>
+   <van-cell
+   :title="text"
+   icon="search"
+   v-for="(text,index) in suggestions"
+   :key="index"
+   ></van-cell>
   </div>
 </template>
 
 <script>
+import { getSearchSuggestions } from '@/api/search'
+
+// 按需加载有好处: 只会把使用到的成员打包到结果中
+import { debounce } from 'lodash'
+
 export default {
   name: 'SearchSuggestion',
   components: {},
-  props: {},
+  props: {
+      searchText: {
+      type: String,
+      required: true
+    }
+  },
   data () {
-    return {}
+    return {
+      suggestions :[] // 联想建议数据列表
+    }
   },
   computed: {},
-  watch: {},
+  watch: {
+    searchText: {
+      // 当 searchText 发生变化的时候就会调用 handler 函数
+      // 注意： <ha></ha>ndler 函数名称是固定的
+      // handler (value){
+      //   // console.log(value);
+      //   this.loadSearchSuggestions(value)
+      // },
+      // debounce 函数
+      // 参数1：一个函数
+      // 参数2：延时时间 单位是毫秒
+      // 返回值：防抖之后的函数
+      handler: debounce(function(value) {
+        // console.log(value);
+        this.loadSearchSuggestions(value)
+      },1000),
+      immediate: true  //该回调将会在侦听开始之后被立即调用
+    }
+  },
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+  async  loadSearchSuggestions (q){
+    try {
+      const { data } = await getSearchSuggestions(q)
+      // console.log(data);
+      this.suggestions = data.data.options
+    } catch (err) {
+      this.$toast('获取数据失败，请稍后重试')
+    }
+    }
+  }
 }
 </script>
 
