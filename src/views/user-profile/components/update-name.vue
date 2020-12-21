ce9c9706800310d8f9e2f801d64aadac7ef50c13
童永泽 <604232797@qq.com>
2020-12-21 11:43:54 +0800

    设置昵称布局完成

---
+++ src/views/user-profile/components/update-name.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="update-name">
+     <!-- 导航栏 -->
+    <van-nav-bar
+  title="设置昵称"
+  left-text="取消"
+  right-text="完成"
+  @click-left="$emit('close')"
+/>
+    <!-- /导航栏 -->
+
+   <div class="field-wrap">
+      <!-- 输入框 -->
+      <van-field
+    v-model="message"
+    rows="2"
+    autosize
+    type="textarea"
+    maxlength="7"
+    placeholder="请输入昵称"
+    show-word-limit
+  />
+    <!-- /输入框 -->
+   </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UpdateName',
+  data () {
+    return {
+      message: ''
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.field-wrap {
+  padding: 20px;
+}
+</style>
---
+++ src/views/user-profile/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+    class="page-nav-bar"
+    title="个人信息"
+    left-arrow
+    @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+
+    <!-- 个人信息 -->
+    <van-cell  title="头像" is-link >
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        :src="user.photo"
+      />
+    </van-cell>
+    <van-cell
+    title="昵称"
+    :value="user.name"
+    is-link
+    @click="isUpdateNameShow = true"
+    />
+    <van-cell
+    title="性别"
+    :value="user.gender === 0 ? '男' : '女'"
+    is-link />
+    <van-cell  title="生日" :value="user.birthday" is-link />
+    <!-- /个人信息 -->
+
+    <!-- 编辑昵称 -->
+    <van-popup
+    v-model="isUpdateNameShow"
+    style="height: 100%"
+    position="bottom"
+    >
+      <update-name @close = "isUpdateNameShow = false"/>
+    </van-popup>
+    <!-- /编辑昵称 -->
+  </div>
+</template>
+
+<script>
+import { getUserProfile } from '@/api/user'
+import UpdateName from './components/update-name'
+
+export default {
+  name: 'UserProfile',
+  components: {
+    UpdateName
+  },
+  data () {
+    return {
+      user: {}, // 个人信息
+      isUpdateNameShow: false
+    }
+  },
+
+  created () {
+    this.loadUserProfile()
+  },
+
+  methods: {
+    async loadUserProfile () {
+      try {
+        const { data } = await getUserProfile()
+        // console.log(data)
+        this.user = data.data
+      } catch (err) {
+        this.$toast('数据获取失败')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+  .van-popup {
+    background-color: #f5f7f9;
+  }
+}
+</style>
